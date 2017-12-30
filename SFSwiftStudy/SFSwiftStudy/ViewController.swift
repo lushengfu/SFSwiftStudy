fb661880984d3910bd8b4b779f9c7671efb4a0c1
lushengfu <cylushengfu@163.com>
2017-12-30 16:33:42 +0800

    swift tip study

--- SFSwiftStudy/SFSwiftStudy/ViewController.swift
+++ SFSwiftStudy/SFSwiftStudy/ViewController.swift
@@ -10,17 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // MARK: life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(SFSingleInstance.shareInstance)
+//        print(SFSingleInstance.shareInstance)
+        for _ in 0...100 {
+//            print(randomInRange(range:1...6 ))
+        }
+       
     }
 
+    // TODO:
+    // FIXME:
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
 
 
+}
+
+extension ViewController {
+    // 实例方法的动态调用
+    func dynamicColled() {
+        let f = MyClass.method  //let f = { (obj: MyClass) in obj.method }
+        let object = MyClass()
+        let result = f(object)(1)
+        print(result)
+        
+        let objectCls = MyClass()
+        print(objectCls.method(number: 2))
+    }
+    
+    func randomInRange(range: Range<Int>) -> Int {
+        let count = UInt32(range.upperBound - range.lowerBound)
+        return  Int(arc4random_uniform(count)) + range.lowerBound
+    }
+    
+}
+
+class MyClass {
+    func method(number: Int) -> Int {
+        return number + 1
+    }
 }
 
